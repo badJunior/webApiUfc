c34ade928217592839cb2cf62bea2ee688bf1a94
Belokrinitskiy <andrey.belokrinitskiy@axelmwd.com>
2024-10-24 04:04:39 +0300

    Cleanup old projects

--- Ufc.Host/InMemoryFightersRepository.cs
+++ Ufc.Host/InMemoryFightersRepository.cs
@@ -2,24 +2,19 @@
 
 public class InMemoryFightersRepository : IFightersRepository
 {
-    private readonly List<string> _fighters;
-
-    public InMemoryFightersRepository()
-    {
-        _fighters = new List<string>
-        {
-            "Makhachev"
-            , "Tsarukyan"
-            , "Oliveira"
-            , "Geitji"
-            , "Porye"
-            , "Gamroth"
-            , "Chandler"
-            , "Dariush"
-            , "Physiology"
-            , "Holloway"
-        };
-    }
+    private readonly List<string> _fighters =
+    [
+        "Makhachev"
+        , "Tsarukyan"
+        , "Oliveira"
+        , "Geitji"
+        , "Porye"
+        , "Gamroth"
+        , "Chandler"
+        , "Dariush"
+        , "Physiology"
+        , "Holloway"
+    ];
 
     public IEnumerable<string> GetFighters()
     {
